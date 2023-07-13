d149f07295c994c09699165a05b5e2e47f1d279e
Greta Va <greta.vasilevskyte@gmail.com>
2023-07-13 21:04:10 +0200

    Update project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,173 @@
 import './App.css';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header';
 
 export default function App() {
+  const baseUrl = 'http://localhost:4000';
+  const [guests, setGuests] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  // const [isAttending, setIsAttending] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // Fetch the guestlist from an API
+
+  async function getList() {
+    const response = await fetch(`${baseUrl}/guests`);
+    const allGuests = await response.json();
+    setGuests(allGuests);
+    // setIsLoading(false);
+    // console.log(allGuests);
+  }
+  useEffect(() => {
+    getList().catch((error) => console.log(error));
+  }, []);
+
+  // console.log(showGuests);
+
+  // when Submit button is clicked:
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+
+  // Add guest from API
+
+  async function addGuest() {
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        // attending: false,
+      }),
+    });
+    const createdGuest = await response.json();
+    // console.log(createdGuest);
+    const guestListCopy = [...guests, createdGuest];
+    setGuests(guestListCopy);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevents the page to reload when submitting the form
+
+    // 👇️ clearing input values after submit
+    setFirstName('');
+    setLastName('');
+  }
+
+  // Update guest status (attending/not attending)
+  async function updateGuestStatus(id, attending) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: !attending }),
+    });
+    const updatedGuest = await response.json();
+    // const guestStatus = [...guests];
+    const updatedList = updatedGuest.filter((guest) => {
+      return guest.id !== updatedGuest.id;
+    });
+    setGuests([...guests], updatedList);
+  }
+
+  // Delete a guest
+
+  async function handleRemove(id) {
+    // console.log(id);
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'DELETE',
+    });
+    const deletedGuest = await response.json();
+    // 1. create a copy
+    // const guestListUpdated = [...guests];
+    console.log(deletedGuest);
+
+    // 2. update the value
+    const newGuestList = guests.filter((guest) => guest.id !== id);
+
+    // 3. set new state
+    setGuests(newGuestList);
+    // console.log(newGuestList);
+  }
+
+  // console.log(guests);
+
+  // removeGuest().catch((error) => {
+  //   console.error(error);
+  // });
+
+  // addGuest().catch((error) => {
+  //   console.log(error);
+  // });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Header />
+
+      <form data-test-id="guest" onSubmit={handleSubmit}>
+        <label>
+          First name
+          <input
+            value={firstName}
+            name="First name"
+            required
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+        <label>
+          Last name
+          <input
+            value={lastName}
+            name="Last name"
+            required
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+
+        <button
+          onClick={(event) => {
+            addGuest().catch((error) => {
+              console.log(error);
+            });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Add guest
+        </button>
+      </form>
+      <>
+        <h1>Guest list</h1>
+        {guests.map((guest) => (
+          <div key={`guest-${guest.id}`} data-test-id="guest">
+            {guest.firstName} {guest.lastName}
+            <button
+              // aria-label={`remove ${guest.firstName}${guest.lastName}`}
+              onClick={() => handleRemove(guest.id)}
+            >
+              Remove
+            </button>
+            <label>
+              Attending?
+              <input
+                aria-label={`attending ${guest.firstName} ${guest.lastName}`}
+                checked={guest.attending}
+                type="checkbox"
+                onChange={(event) => {
+                  updateGuestStatus(guest.id).catch((error) =>
+                    console.log(error),
+                  );
+                }}
+              />
+              <span>
+                {guest.attending === true ? 'attending' : 'not attending'}
+              </span>
+            </label>
+          </div>
+        ))}
+      </>
+    </>
   );
 }
---
+++ src/Draft1.js
@@ -0,0 +1,87 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+import Form from './components/Form';
+import GuestListVisible from './components/GuestListVisible';
+import Header from './components/Header';
+
+export default function App() {
+  const baseUrl = 'http://localhost:4000';
+  const [showGuests, setShowGuests] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // Fetch the guestlist from an API
+  //
+  async function getList() {
+    const response = await fetch(`${baseUrl}/guests/`);
+    const allGuests = await response.json();
+    setShowGuests(allGuests);
+    // setIsLoading(false);
+    // console.log(allGuests);
+  }
+  useEffect(() => {
+    getList().catch((error) => console.log(error));
+  }, []);
+
+  // console.log(showGuests);
+
+  // when Submit button is clicked:
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+
+  // Add guest from API
+
+  async function getGuest() {
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        attending: false,
+      }),
+    });
+    const createdGuest = await response.json();
+    console.log(createdGuest);
+    setShowGuests(createdGuest);
+  }
+
+  // async function handleSubmit(event) {
+  //   await getGuest();
+  //   event.preventDefault();
+  // }
+
+  // getGuest().catch((error) => {
+  //   console.log(error);
+  // });
+
+  // const addGuest = (text) => {
+  //   let id = 1;
+  //   if (showGuests.length > 0) {
+  //     id = showGuests[0].id + 1;
+  //   }
+  //   let showGuest = { id: id, text: text };
+  // //   let newGuests = [showGuest, ...showGuests];
+  //   console.log(newGuests);
+  //   setShowGuests(newGuests);
+  // };
+
+  return (
+    <>
+      {/* 1. Header */}
+      <Header />
+      {/* 2. Input form and button */}
+      <Form
+        firstName={firstName}
+        setFirstName={setFirstName}
+        lastName={lastName}
+        setLastName={setLastName}
+      />
+      {/* 3. List of guests */}
+      <GuestListVisible showGuests={showGuests} setShowGuests={setShowGuests} />
+    </>
+  );
+}
